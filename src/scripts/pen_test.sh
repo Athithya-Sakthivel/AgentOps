7fce6583854d2abad69e02d55a29613384779d60
Athithya-Sakthivel <athithya651@gmail.com>
2026-06-04 01:27:53 +0000

    fixing pen testing

--- src/scripts/pen_test.sh
+++ src/scripts/pen_test.sh
@@ -1,5 +1,5 @@
 
-
+gh secret set TEST_ACCOUNT_EMAIL --body athithya851@gmail.com
 gh secret set TEST_ACCOUNT_PASSWORD --body $TEST_ACCOUNT_PASSWORD
 aws ssm put-parameter --name "/agentops/admin-allowed-google-domains" --type "String" --value "gmail.com" --overwrite --region ap-south-1
 bash src/offline/commands.sh
